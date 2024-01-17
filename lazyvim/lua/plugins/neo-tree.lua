5138574abd86df3d57e094aaed6008d76b8311f4
Bulkan Evcimen <bulkan@mini.fritz.box>
2024-01-17 16:22:37 +1100

    Update neotree key bindings

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,6 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	keys = {
 		{ "<leader>n", "<cmd>Neotree position=current float reveal toggle<cr>", desc = "Toggle file tree" },
-		-- { "<leader>nn", "<cmd>Neotree position=current toggle ~/Code/bulkan/notes/2024 <cr>", desc = "Toggle file tree" },
+		{ "-", "<cmd>Neotree position=current float reveal toggle<cr>", desc = "Toggle file tree" },
 	},
 }
