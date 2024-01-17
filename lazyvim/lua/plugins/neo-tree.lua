37500396e44bf9873f036689add47e5e68396495
Bulkan Evcimen <bulkan@gmail.com>
2024-01-17 16:21:30 +1100

    Add more plugins

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,7 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  keys = {
-    { "<leader>n", "<cmd>Neotree position=current float reveal toggle<cr>", desc = "Toggle file tree" },
-    -- { "<leader>nn", "<cmd>Neotree position=current toggle ~/Code/bulkan/notes/2024 <cr>", desc = "Toggle file tree" },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{ "<leader>n", "<cmd>Neotree position=current float reveal toggle<cr>", desc = "Toggle file tree" },
+		-- { "<leader>nn", "<cmd>Neotree position=current toggle ~/Code/bulkan/notes/2024 <cr>", desc = "Toggle file tree" },
+	},
 }
---
+++ lazyvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,14 @@
+return {
+	"kevinhwang91/nvim-ufo",
+
+	dependencies = { "kevinhwang91/promise-async" },
+
+	init = function()
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+	end,
+
+	config = function()
+		require("ufo").setup({})
+	end,
+}
