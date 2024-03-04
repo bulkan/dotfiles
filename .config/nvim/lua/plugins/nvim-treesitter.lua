6a80e64745678d741102027b593bfe7086d6324f
Bulkan Evcimen <bulkan@gmail.com>
2024-03-04 20:25:22 +1100

    nvim: Add tresitter-modules, especially kdl

---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "kdl",
+      "kotlin",
+      "styled",
+      "rust",
+      "ruby",
+      "jsonnet",
+      "yaml",
+    },
+  },
+}
